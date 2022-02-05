2ab04d09745357e663801da810a3a8dcba5fd67f
Matthew Lindfield Seager <matthew@sagacity.com.au>
2022-02-05 15:12:48 +0700

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    './app/views/**/*.html.erb',
+    './app/helpers/**/*.rb',
+    './app/javascript/**/*.js',
+  ],
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/forms'),
+  ],
+}
