595d99f469bd8f28bd61400424484a7b1289cd44
Matthew Lindfield Seager <matthew@sagacity.com.au>
2022-02-23 07:40:29 +0700

    Fix bug when listing events with no results
     - Use safe navigation operator when first event is nil
     - Update results helper to handle nil results

--- app/helpers/results_helper.rb
+++ app/helpers/results_helper.rb
@@ -1,5 +1,6 @@
 module ResultsHelper
   def time_as_string(time)
+    return "Unknown" unless time
     format_string = (time < 3600 ? "%M:%S" : "%H:%M:%S")
     Time.at(time).utc.strftime(format_string)
   end
