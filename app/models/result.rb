002b9d964a1e60f8130633574bbe267e661e2033
Matthew LS <matt17r@users.noreply.github.com>
2022-02-27 14:21:18 +0700

    Add results pages for individuals (#1)
     - Link from result to person
     - Show PBs on individual and event result pages
     - Add tooltips to explain PB bell

--- app/models/result.rb
+++ app/models/result.rb
@@ -8,4 +8,13 @@ class Result < ApplicationRecord
     less_than_or_equal_to: ResultsHelper::MAXIMUM_TIME,
     allow_nil: true
   }
+
+  def place
+    event.results.where("time < ?", time).count + 1
+  end
+
+  def pb?
+    previous_best = person.results.order(:time).joins(:event).where("events.number < ?", event.number).first&.time
+    time && previous_best && time < previous_best
+  end
 end
