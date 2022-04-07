249f55d1d59a7bf939e33741f57b57477df67bd5
Matthew Lindfield Seager <matthew@sagacity.com.au>
2022-04-07 22:06:36 +0700

    Standardise emoji error messages

--- app/models/person.rb
+++ app/models/person.rb
@@ -8,7 +8,7 @@ class Person < ApplicationRecord
 
   validates :name, presence: true
   validates :email, format: {with: URI::MailTo::EMAIL_REGEXP, message: "must contain a correctly formatted email address"}
-  validates :emoji, allow_blank: true, format: {with: Unicode::Emoji::REGEX_VALID, message: "can only contain valid emoji sequences"}
+  validates :emoji, allow_blank: true, format: {with: Unicode::Emoji::REGEX_VALID, message: "must be a single emoji, must be from the list of RGI (recommended) emojis and must be in emoji, not text, presentation"}
   validate :emoji_length
   validate :emoji_recommended
 
@@ -27,14 +27,14 @@ class Person < ApplicationRecord
 
   def emoji_length
     if emoji.present? && emoji.grapheme_clusters.length > 1
-      errors.add(:emoji, "can only contain 1 emoji")
+      errors.add(:emoji, "must be a single emoji, must be from the list of RGI (recommended) emojis and must be in emoji, not text, presentation")
     end
   end
 
   def emoji_recommended
     return if PROBLEMATIC_EMOJI_IN_UNICODE_EMOJI_GEM_V3_1_0.include? emoji # Temporary short circuit on problematic emoji - https://github.com/janlelis/unicode-emoji/issues/12
     if emoji.present? && emoji.scan(Unicode::Emoji::REGEX_VALID).present? && emoji.scan(Unicode::Emoji::REGEX_VALID)[0].length > emoji.scan(Unicode::Emoji::REGEX)[0].length
-      errors.add(:emoji, "is a valid sequence but is not a recommended combination")
+      errors.add(:emoji, "must be a single emoji, must be from the list of RGI (recommended) emojis and must be in emoji, not text, presentation")
     end
   end
 
