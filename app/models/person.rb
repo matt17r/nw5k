5785615abd6381d99f8ec1c15eb17ebb052de9fc
Matthew Lindfield Seager <matthew@sagacity.com.au>
2022-04-09 13:55:38 +0700

    Update Emoji validations

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,5 +1,6 @@
 class Person < ApplicationRecord
   PROBLEMATIC_EMOJI_IN_UNICODE_EMOJI_GEM_V3_1_0 = ["👨‍👩‍👧‍👦", "👨‍👩‍👦‍👦", "👨‍👩‍👧‍👧", "👨‍👨‍👧‍👦", "👨‍👨‍👦‍👦", "👨‍👨‍👧‍👧", "👩‍👩‍👧‍👦", "👩‍👩‍👦‍👦", "👩‍👩‍👧‍👧", "👨‍👦‍👦", "👨‍👧‍👦", "👨‍👧‍👧", "👩‍👦‍👦", "👩‍👧‍👦", "👩‍👧‍👧"]
+  EMOJI_REQUIREMENTS = "(if present) must be a single emoji, must be from the list of RGI (recommended) emojis and must be in emoji, not text, presentation (e.g. ✌️ not ✌︎)".freeze
 
   before_validation :strip_spaces
   before_save :downcase_email
@@ -8,9 +9,9 @@ class Person < ApplicationRecord
 
   validates :name, presence: true
   validates :email, format: {with: URI::MailTo::EMAIL_REGEXP, message: "must contain a correctly formatted email address"}
-  validates :emoji, allow_blank: true, format: {with: Unicode::Emoji::REGEX_VALID, message: "must be a single emoji, must be from the list of RGI (recommended) emojis and must be in emoji, not text, presentation"}
-  validate :emoji_length
-  validate :emoji_recommended
+  validates :emoji, allow_blank: true, format: {with: Unicode::Emoji::REGEX_VALID, message: EMOJI_REQUIREMENTS}
+  validate :single_emoji_only
+  validate :recommended_emoji_only
 
   has_secure_password
   has_secure_token :remember_token
@@ -25,16 +26,16 @@ class Person < ApplicationRecord
     self.email = email.downcase
   end
 
-  def emoji_length
+  def single_emoji_only
     if emoji.present? && emoji.grapheme_clusters.length > 1
-      errors.add(:emoji, "must be a single emoji, must be from the list of RGI (recommended) emojis and must be in emoji, not text, presentation")
+      errors.add(:emoji, EMOJI_REQUIREMENTS)
     end
   end
 
-  def emoji_recommended
+  def recommended_emoji_only
     return if PROBLEMATIC_EMOJI_IN_UNICODE_EMOJI_GEM_V3_1_0.include? emoji # Temporary short circuit on problematic emoji - https://github.com/janlelis/unicode-emoji/issues/12
     if emoji.present? && emoji.scan(Unicode::Emoji::REGEX_VALID).present? && emoji.scan(Unicode::Emoji::REGEX_VALID)[0].length > emoji.scan(Unicode::Emoji::REGEX)[0].length
-      errors.add(:emoji, "must be a single emoji, must be from the list of RGI (recommended) emojis and must be in emoji, not text, presentation")
+      errors.add(:emoji, EMOJI_REQUIREMENTS)
     end
   end
 
