546286194f5d7214fa21af11f6ac734bab23e406
Matthew Lindfield Seager <matthew@sagacity.com.au>
2022-04-16 15:06:29 +0700

    Still testing

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,11 +5,12 @@ class SessionsController < ApplicationController
   def create
     @admin = Admin.find_by(email: params[:admin][:email].downcase)
     if @admin&.authenticate(params[:admin][:password])
+      cached_after_login_path = session[:return_to] || root_path
       login @admin
       remember(@admin) if params[:admin][:rememember_me] == "1"
       puts "SessionsController.create:"
-      puts "/tsession[:return_to] = #{session[:return_to]}"
-      redirect_to session[:return_to] || root_path, notice: "Signed in."
+      puts "cached_after_login_path = #{cached_after_login_path}"
+      redirect_to cached_after_login_path, notice: "Signed in."
     else
       flash.now[:alert] = "Incorrect email or password."
       render :new, status: :unprocessable_entity
