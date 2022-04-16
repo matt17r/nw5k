6039f7a6dc570f7858d657a3f024265ebfc70207
Matthew Lindfield Seager <matthew@sagacity.com.au>
2022-04-16 15:20:20 +0700

    Actually inspecting the request...

--- app/controllers/concerns/authentication.rb
+++ app/controllers/concerns/authentication.rb
@@ -43,7 +43,8 @@ module Authentication
     puts "  request.get? = #{request.get?}"
     puts "  request.local? = #{request.local?}"
     puts("*" * 30)
-    puts "request.inspect"
+    puts "request.inspect:"
+    puts request.inspect
     puts("*" * 30)
   end
 
