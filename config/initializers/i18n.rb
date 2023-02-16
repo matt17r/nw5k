35e9571b3287049152b855b58eb7198cba437da2
Matthew LS <matt17r@users.noreply.github.com>
2023-02-16 23:21:00 +0700

    Rebranding and language switcher (#5)
    New logo 🥳
    
    * Add new logo to header
    * Update OG image
    * Update website colours to match
    * Add language switcher
    * Make Khmer the default
    * Add PB distance labels
    * Update default font weight, sizes and spacing
    * Alphabetise CSS

--- config/initializers/i18n.rb
+++ config/initializers/i18n.rb
@@ -1,5 +1,5 @@
 I18n.load_path += Dir[Rails.root.join("config", "locales", "**", "*.yml")]
 
-I18n.available_locales = %i[en km ja zh]
+I18n.available_locales = %i[en km]
 
-I18n.default_locale = :en
+I18n.default_locale = :km
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,36 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: [
-    './app/views/**/*.html.erb',
+    './public/*.html',
     './app/helpers/**/*.rb',
     './app/javascript/**/*.js',
+    './app/views/**/*.{erb,haml,html,slim}'
   ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        'nw5k-eggplant': {
+          900: '#301F29',
+          800: '#523546',
+          700: '#704A60',
+          600: '#8C5F7A',
+          500: '#A67491',
+          400: '#BD8AA8',
+          300: '#D1A1BD',
+          200: '#E3BAD2',
+          100: '#F2D8E7',
+           50: '#FFF5FB',
+        },
+        'nw5k-teal': '#28525b',
+      },
+    },
+  },
   plugins: [
-    require('@tailwindcss/typography'),
     require('@tailwindcss/forms'),
-  ],
+    require('@tailwindcss/typography'),
+  ]
 }
