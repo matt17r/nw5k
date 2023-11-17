59f581511b090f7cd418b5b713e6fd7a4fc6c6a5
Matthew Lindfield Seager <matt@matt17r.com>
2023-11-17 12:39:22 +1100

    Turn off profiling in production
    Remove Rack Mini Profiler and turn it off

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,7 @@
 class ApplicationController < ActionController::Base
   include Authentication
   before_action :authenticate_admin!
-  before_action :check_rack_mini_profiler
   before_action :set_locale
-
-  def check_rack_mini_profiler
-    if admin_signed_in?
-      Rack::MiniProfiler.authorize_request
-    end
-  end
 
   def set_locale
     I18n.locale = params[:locale] || I18n.default_locale
